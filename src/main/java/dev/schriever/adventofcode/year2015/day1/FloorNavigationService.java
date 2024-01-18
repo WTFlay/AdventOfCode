b98ccc6eaa6177a878dd73c0e18dbf49e4b9ae6f
Flavien Schriever <flavien@schriever.dev>
2024-01-18 16:03:14 +0100

    refactor year2025/day1/part2

--- src/main/java/dev/schriever/adventofcode/year2015/day1/FloorNavigationService.java
+++ src/main/java/dev/schriever/adventofcode/year2015/day1/FloorNavigationService.java
@@ -1,28 +1,35 @@
 package dev.schriever.adventofcode.year2015.day1;
 
 public class FloorNavigationService {
+  private static final long INITIAL_FLOOR = 0;
+  private static final long BASEMENT_FLOOR = -1;
   private static final char UP_CHAR = '(';
   private static final char DOWN_CHAR = ')';
 
   public long detectFloor(String input) {
-    long upFloors = input.chars().filter(this::isUpFloorChar).count();
-    long downFloors = input.chars().filter(this::isDownFloorChar).count();
-    return upFloors - downFloors;
+    return input.chars().mapToLong(this::toFloorDirection).reduce(INITIAL_FLOOR, Long::sum);
   }
 
   public long detectBasementEntryPosition(String input) {
-    long actualFloor = 0;
-    for (int i = 0; i < input.length(); i++) {
-      if (isUpFloorChar(input.charAt(i))) {
-        actualFloor += 1;
-      } else if (isDownFloorChar(input.charAt(i))) {
-        actualFloor -= 1;
-      }
-      if (actualFloor == -1) {
-        return i + 1;
+    long actualFloor = INITIAL_FLOOR;
+    for (int pos = 0; pos < input.length(); pos++) {
+      char currentChar = input.charAt(pos);
+      actualFloor += toFloorDirection(currentChar);
+      if (actualFloor == BASEMENT_FLOOR) {
+        return pos + 1;
       }
     }
-    return 0;
+    throw new RuntimeException("We never reach the basement");
+  }
+
+  private long toFloorDirection(int ch) {
+    if (isUpFloorChar(ch)) {
+      return 1;
+    } else if (isDownFloorChar(ch)) {
+      return -1;
+    } else {
+      return 0;
+    }
   }
 
   private boolean isUpFloorChar(int ch) {
