44e4fa51e0f3e84e23b315c3746a87b9a0b5bbe1
Flavien Schriever <flavien@schriever.dev>
2024-01-22 15:26:41 +0100

    Day2#refactor: add value object NumberOfBalls

--- src/main/java/dev/schriever/adventofcode/day2/GameParser.java
+++ src/main/java/dev/schriever/adventofcode/day2/GameParser.java
@@ -18,20 +18,26 @@ public class GameParser {
     List<Round> roundList = new ArrayList<>();
     for (String roundStr : parts[1].split(";")) {
       String[] colors = roundStr.split(",");
-      Map<Color, Integer> colorMap = new HashMap<>();
-      for (String colorStr : colors) {
-        String total = digitFinder.findFirstDigit(colorStr);
-        Color color = findColor(colorStr);
-        colorMap.put(color, Integer.valueOf(total));
-      }
+      Map<Color, NumberOfBalls> colorMap = parseNumberOfBallsForEachColors(colors);
       roundList.add(new Round(
-          colorMap.getOrDefault(Color.RED, 0),
-          colorMap.getOrDefault(Color.GREEN, 0),
-          colorMap.getOrDefault(Color.BLUE, 0)
+          colorMap.getOrDefault(Color.RED, NumberOfBalls.ZERO).number(),
+          colorMap.getOrDefault(Color.GREEN, NumberOfBalls.ZERO).number(),
+          colorMap.getOrDefault(Color.BLUE, NumberOfBalls.ZERO).number()
       ));
     }
 
     return new Game(Long.valueOf(gameNumber), roundList);
+  }
+
+  private Map<Color, NumberOfBalls> parseNumberOfBallsForEachColors(String[] colors) {
+    Map<Color, NumberOfBalls> colorMap = new HashMap<>();
+    for (String colorStr : colors) {
+      Color color = findColor(colorStr);
+      String firstDigit = digitFinder.findFirstDigit(colorStr);
+      NumberOfBalls numberOfBalls = new NumberOfBalls(Integer.parseInt(firstDigit));
+      colorMap.put(color, numberOfBalls);
+    }
+    return colorMap;
   }
 
   private Color findColor(String text) {
@@ -42,5 +48,9 @@ public class GameParser {
   }
 
   private enum Color {BLUE, RED, GREEN}
+
+  record NumberOfBalls(int number) {
+    static NumberOfBalls ZERO = new NumberOfBalls(0);
+  }
 
 }
